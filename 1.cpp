85f1298d324a1a947c390497f96cdd5ef249be9c
tomlem713 <t.leman@przydatnydron.pl>
2022-01-03 12:10:19 +0100

    dodoajemy pierwszy test

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+
+
+
+int main()
+{
+cout <<"ggggg";
+
+
+
+
+}
